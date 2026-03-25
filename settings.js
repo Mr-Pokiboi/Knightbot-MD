05e42d876a7cdc65d752e7e578f1c174eeb44e44
ibrahim rai <raiibrahim188@gmail.com>
2026-03-25 15:28:18 +0300

    Update botOwner name in settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Ibrahtech-Bot', // Your name
+  botOwner: 'Ibrahtech Bot', // Your name
   ownerNumber: '254794122835', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
