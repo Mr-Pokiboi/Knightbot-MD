27f5993ee9fbcb1d7351717c430c7dd3431079f3
ibrahim rai <raiibrahim188@gmail.com>
2026-04-08 20:44:07 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Ibrahtech Bot', // Your name
-  ownerNumber: '254794122835', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'IbrahBot', // Your name
+  ownerNumber: '254708798061', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
